a145cdd34d535f86f10029cf8b87de56cf90ba61
marcelo <marcelobbff@gmail.com>
2018-02-05 16:27:49 -0200

    removed add_block for mine_pending_transactions added new examples and more comments added transactions and  mining blocks for pending_transactions added reward for miner

--- blockchain.py
+++ blockchain.py
@@ -2,16 +2,21 @@ import time
 import hashlib
 
 
+class Transaction:
+    def __init__(self, sender_address, receiver_address, amount):
+        self.sender_address = sender_address
+        self.receiver_address = receiver_address
+        self.amount = amount
+
+
 class Block:
-    def __init__(self, index, data, previous_hash=''):
-        # Block id
-        self.index = index
+    def __init__(self, transactions, previous_hash=''):
 
         # stores the hash of the previous block
         self.previous_hash = previous_hash
 
         # Stores data
-        self.data = data
+        self.transactions = transactions
 
         # Incremented for mining blocks
         self.nonce = 0
@@ -23,7 +28,11 @@ class Block:
         self.hash = self.calculate_hash()
 
     def calculate_hash(self):
-        params = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash) + str(self.nonce)
+
+        # Concatenate all attributes to build the block's hash
+        params = str(self.timestamp) + str(self.transactions) + str(self.previous_hash) + str(self.nonce)
+
+        # All attributes are hashed in sha256 and
         return str(hashlib.sha256(params.encode("ascii")).hexdigest())
 
     #   print a blocks attributes
@@ -36,6 +45,7 @@ class Block:
 
     #   Generates hashes until a hash is found respecting the dificulty condition
     def mine_block(self, dificulty):
+
         print("mining...")
 
         # Condition: The first 'dificulty' digits must be 0's
@@ -61,34 +71,51 @@ class BlockChain:
         # Initialise the Genesis Block of our BlockChain
         # Genesis Block is the first Block and have no previous block
         # and doesn't have a previous hash.
-        self.chain = [Block(0, "Genesis Block")]
+        self.chain = [Block([], 0)]  # "Genesis Block"
 
-        # Set a dificulty limitation level for each hash.
-        # The dificulty will set the number of 0's that must appear as begining character in a hash.
+        # Set a difficulty limitation level for each hash.
+        # The difficulty will set the number of 0's that must appear as begining character in a hash.
         # Ex: If dificulty=4, the first 4 characters of a hash must be 0's. "0000..."
-        # Currently bitcoin dificulty is 18.
-        self.dificulty = 2
+        # Currently bitcoin difficulty is 18.
+        self.dificulty = 3
 
+        # Stores an array of transactions to add in the next block
+        self.pending_transactions = []
+
+        # miner reward for each block
+        self.mining_reward = 100.00
+
+    # Get the last block from blockchain
     def get_latest_block(self):
         return self.chain[len(self.chain) - 1]
 
-    def add_block(self, new_block):
-        # get and add previous block to current block
-        new_block.previous_hash = self.get_latest_block().hash
+    def mine_pending_transactions(self, miner_address):
 
-        # discover a hash for this current block
-        new_block.mine_block(self.dificulty)
+        # Creates a new block and add's pending_transactions array to be mined and previous block hash
+        block = Block(self.pending_transactions, self.get_latest_block().hash)
 
-        # Add the new block to the BlockChain
-        self.chain.append(new_block)
+        # mine appropriate hash with the blockchain difficulty
+        block.mine_block(self.dificulty)
+        print("Block successfully mined!")
 
+        # Add block to the blockchain
+        self.chain.append(block)
+
+        # Reset pending transactions and add transaction with miner reward to next transaction array
+        self.pending_transactions = [Transaction(None, miner_address, self.mining_reward)]
+
+    # Adds a new transaction to pending_transaction array
+    def create_transactions(self, transaction):
+        self.pending_transactions.append(transaction)
+
+    # Print all block in the blockchain
     def show_chain(self):
 
         for block in self.chain:
             print("{")
             print("index: ", block.index)
             print("timestamp: ", block.timestamp)
-            print("data: ", block.data)
+            print("transactions: ", block.transactions)
             print("hash: ", block.hash)
             print("previous_hash", block.previous_hash)
             print("}\n",)
@@ -106,6 +133,22 @@ class BlockChain:
 
         return True
 
+    def get_balance_of_address(self, address):
+        # Address balance are not stored in the blockchain so it was to be calculated.
+        balance = 0
+
+        # To access your amount you must go trough all transactions in the blockchain
+        for block in self.chain:
+            for transaction in block.transactions:
+
+                if transaction.sender_address == address:
+                    balance = balance - transaction.amount
+
+                if transaction.receiver_address == address:
+                    balance = balance + transaction.amount
+
+        return balance
+
 
 def main():
 
@@ -113,13 +156,19 @@ def main():
     # creates BlockChain
     marcelo_coin = BlockChain()
 
-    # Creates new blocks
-    b1 = Block(1, {"amount": 20, "Sender": "Marcelo", "Receiver": "Pedro"})
-    b2 = Block(1, {"amount": 15, "Sender": "Matheus", "Receiver": "Filipe"})
+    # Creates transactions
+    marcelo_coin.create_transactions(Transaction("marcelo_pub_key", "pedro_pub_key", 150))
+    marcelo_coin.create_transactions(Transaction("matheus_pub_key", "fijo_pub_key", 24))
 
-    # Add created blocks to my BlockChain
-    marcelo_coin.add_block(b1)
-    marcelo_coin.add_block(b2)
+    # All transactions are stored in pending_transactions array
+    # To process these transactions it's necessary to mine a block to process them
+
+    # Starting the miner
+    marcelo_coin.mine_pending_transactions("mendigo_pub_key")
+
+    # Print fijo's balance
+    amount = marcelo_coin.get_balance_of_address("fijo_pub_key")
+    print("Fijo's balance: ", amount)
 
     # Print extisting blocks in BlockChain
     # marcelo_coin.show_chain()
